ebb364d0d21f8bfeef64332b2a4770b393063f22
didiermeow <jorgedidier149@gmail.com>
2026-09-18 14:17:54 -0600

    Update staticobj.lua

--- game/classes/staticobj.lua
+++ game/classes/staticobj.lua
@@ -1,15 +1,15 @@
-local baseobj = require("classes.baseobj")
+-- local baseobj = require("classes.baseobj")
 
-local staticobj = baseobj:extend()
+-- local staticobj = baseobj:extend()
 
-function staticobj:new(x,y,w,h,r,img)
-  staticobj.super.new(self,x,y,w,h)
-  self.img = img
-  self.r = r
-end
+-- function staticobj:new(x,y,w,h,r,img)
+--   staticobj.super.new(self,x,y,w,h)
+--   self.img = img
+--   self.r = r
+-- end
 
-function staticobj:draw()
-  love.graphics.draw(self.img,self.x,self.y,self.r,self.w,self.h)
-end
+-- function staticobj:draw()
+--   love.graphics.draw(self.img,self.x,self.y,self.r,self.w,self.h)
+-- end
 
-return staticobj
+-- return staticobj
